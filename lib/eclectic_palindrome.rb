8711a54e59c176a9f46815ece684fa1eb0e38585
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-05-20 21:46:43 -0400

    Initial commit

---
+++ lib/eclectic_palindrome.rb
@@ -0,0 +1,6 @@
+require "eclectic_palindrome/version"
+
+module EclecticPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/eclectic_palindrome/version.rb
@@ -0,0 +1,3 @@
+module EclecticPalindrome
+  VERSION = "0.1.0"
+end
